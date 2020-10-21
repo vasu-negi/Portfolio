34bc1fd470a8c1972cd37d8760fa14c1a531ca5a
vasu-negi <vasu.negi1995@gmail.com>
2020-10-20 21:24:15 -0400

    footer change

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -26,7 +26,7 @@ const Footer = () => {
               return (
                 <a
                   key={id}
-                  href={url || 'https://github.com/cobidev/gatsby-simplefolio'}
+                  href={url || 'https://github.com/vasu-negi'}
                   rel="noopener noreferrer"
                   target="_blank"
                   aria-label={name}
@@ -39,8 +39,8 @@ const Footer = () => {
         <hr />
         <p className="footer__text">
           © {new Date().getFullYear()} - Template developed by{' '}
-          <a href="https://github.com/cobidev" target="_blank" rel="noopener noreferrer">
-            Jacobo Martínez
+          <a href="https://github.com/vasu-negi" target="_blank" rel="noopener noreferrer">
+            Vasu Negi
           </a>
         </p>
 
