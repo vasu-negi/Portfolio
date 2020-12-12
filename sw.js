0d8297f271867dd23f338654f892dea2fe8ee130
vasu-negi <vasu.negi1995@gmail.com>
2020-12-12 11:46:18 -0500

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-3613cb3d3d0a6b3d4fa5.js"
+    "url": "webpack-runtime-b3be8f9cee1670c6099c.js"
   },
   {
     "url": "framework-dac95de3c301f57a114a.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b6f895eeedb0ee8ca0c4e31e516de208"
+    "revision": "b0ff394ff8163cb640cdcc8521486973"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
